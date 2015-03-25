bcc595de70c42696d918388d128b2e44a2d0038d
jinye318 <jinye318@naver.com>
2015-03-25 14:40:51 +0900

    reset

--- src/test2_dir2/test2.java
+++ src/test2_dir2/test2.java
@@ -5,6 +5,9 @@ public class test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is test in dir2");
+		System.out.println("branch1");
+		System.out.println("This is test in dir2");
+		
 	}
 
 }
